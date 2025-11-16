7cbdc28daf68d56980a844264f0082fd5aca4da3
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-11-15 21:56:33 -0800

    dotenc configed and port instedted

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.listen(2000, () => {
-    console.log("App is Running on port 200")
+
+app.listen(PORT, () => {
+    console.log(`App is Running on port ${PORT}`)
 });
